076253cc67b5e3fb57c18ab6a4fb6c49c2e51ebe
FY-艾斯 <root@2kil.com>
2024-05-15 11:28:48 +0800

    新增错误检测,输出编译时间

--- 2kStar.go
+++ 2kStar.go
@@ -2,7 +2,7 @@
  * @Author: 2Kil
  * @Date: 2024-04-19 10:54:20
  * @LastEditors: 2Kil
- * @LastEditTime: 2024-04-19 15:47:31
+ * @LastEditTime: 2024-05-15 11:26:09
  * @Description:star
  */
 package star
@@ -13,16 +13,15 @@ import (
 	"log"
 	"math/rand"
 	"net"
+	"os"
 	"os/exec"
 	"strings"
 	"sync/atomic"
 	"time"
 )
 
-var counter int64
-
 func IsBug() {
-	log.Println("hello")
+	log.Println("The current mode is debug")
 }
 
 /**
@@ -31,6 +30,7 @@ func IsBug() {
  * @return {int} 随机数
  */
 func RandAtomic(max int) int {
+	var counter int64
 	rand.New(rand.NewSource(time.Now().UnixNano() + atomic.AddInt64(&counter, 1)))
 	randomNum := rand.Intn(max)
 	return randomNum
@@ -72,4 +72,37 @@ func GetSerialKey() string {
 
 	// 简化设备码
 	return reg0[8:11] + reg0[2:3] + reg0[12:14]
+}
+
+/**
+ * @description: 错误检测
+ * @param {error} err 错误信息
+ * @param {string} errString 自定义错误提示
+ * @return {*}
+ */
+func CheckErr(err error, errString ...string) {
+	errString = append(errString, "Error")
+	if err != nil {
+		log.Println(errString, err)
+	}
+}
+
+/**
+ * @description: 输出编译时间
+ * @return {string} 当前文件编译时间
+ */
+func BuildTime() string {
+	// 获取当前程序的文件信息
+	fileInfo, err := os.Stat(os.Args[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 获取修改时间
+	modTime := fileInfo.ModTime()
+	buildTime := modTime.Format("06.0102.1504")
+
+	// 打印修改时间
+	log.Printf("Build Time:%s", buildTime)
+	return buildTime
 }
